e1450494908fc8bc6fb1ad9e286a0658f29ce995
Carlos-Henrique-Lins-Ponchirolli <ch.ponchirolli@gmail.com>
2023-09-17 20:05:25 -0300

    Pop up funcionando

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,28 @@
-function toggleComment(commentId) {
-            const comment = document.querySelector(`#${commentId}`);
-            if (comment.style.height === 'auto') {
-                comment.style.height = '100px'; // Defina o tamanho fixo desejado
-            } else {
-                comment.style.height = 'auto'; // Expandir para o tamanho total
-            }
-        }
+const popup = document.getElementById("pop-up");
+const popup1 = document.querySelector("#pop-up-1"); 
+const popup2 = document.getElementById("popup2");
+const open = document.getElementById("open-popup-btn");
+const close = document.getElementById("close")
+
+open.addEventListener("click",()=>{
+    popup1.classList.remove("none");
+    popup1.classList.add("pop-up-1");
+    open.classList.add("none");
+    popup.classList.add("pop-up");
+})
+
+close.addEventListener("click",()=>{
+    popup1.classList.add("none");
+    popup1.classList.remove("pop-up-1");
+    open.classList.remove("none");
+    popup.classList.remove("pop-up");
+})
+
+function openPopup(){
+    popup2.classList.add("open-popup")
+}
+
+function closePopup(){
+    popup2.classList.remove("open-popup")
+}
+
