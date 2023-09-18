efdf5f55609f3dfcdd74290cb8f2b4bb030b0cc0
Carlos-Henrique-Lins-Ponchirolli <ch.ponchirolli@gmail.com>
2023-09-18 20:26:14 -0300

    Script arrumado

--- JS/script.js
+++ JS/script.js
@@ -28,8 +28,8 @@ if (exampleModal2) {
     // and then do the updating in a callback.
 
     // Update the modal's content.
-    const modalTitle2 = exampleModal.querySelector('.modal-title-2')
-    const modalBodyInput2 = exampleModal.querySelector('.modal-body-2 input')
+    const modalTitle2 = exampleModal2.querySelector('.modal-title-2')
+    const modalBodyInput2 = exampleModal2.querySelector('.modal-body input')
 
     modalTitle2.textContent = `Compartilhe sua sugestão`
     modalBodyInput2.value = recipient2
