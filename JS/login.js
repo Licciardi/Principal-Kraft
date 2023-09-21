f4b8e82ffd0e0cbe5c2d1ad0d964d47e1f0b44bd
Carlos Henrique Lins Ponchirolli <107359480+CarlosHLP060304@users.noreply.github.com>
2023-09-21 12:48:45 -0300

    Add files via upload

---
+++ JS/login.js
@@ -0,0 +1,22 @@
+const senha  = document.querySelector("#senha")
+const confirm_senha = document.querySelector("#senha_confirm")
+const msg_senha_diferente = document.querySelector("#msg_senha_diferente")
+const container_data = document.querySelector("#container-data")
+const tipo_cliente = document.querySelectorAll(".tipo_cliente")
+console.log(tipo_cliente)
+
+confirm_senha.addEventListener("blur",()=>{
+    if(senha.value!=confirm_senha.value){
+        msg_senha_diferente.textContent = "Erro: senhas diferentes. Digite a mesma senha em ambos os campos de senha!!!"
+        confirm_senha.value = ""
+    }
+})
+
+tipo_cliente.forEach((campo)=>{campo.addEventListener("click",()=>{
+    console.log(campo.value)
+    if(campo.value == "empresa"){
+        container_data.classList.add("none")
+    }else{
+        container_data.classList.remove("none")
+    }
+})})
--- JS/script.js
+++ JS/script.js
@@ -12,7 +12,7 @@ if (exampleModal) {
     const modalTitle = exampleModal.querySelector('.modal-title')
     const modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-    modalTitle.textContent = `Compartilhe sua satifação`
+    modalTitle.textContent = `Sua opinião conta. Avalie nosso serviço e nos ajude a crescer!`
     modalBodyInput.value = recipient
   })
 }
@@ -28,10 +28,10 @@ if (exampleModal2) {
     // and then do the updating in a callback.
 
     // Update the modal's content.
-    const modalTitle2 = exampleModal.querySelector('.modal-title-2')
-    const modalBodyInput2 = exampleModal.querySelector('.modal-body-2 input')
+    const modalTitle2 = exampleModal2.querySelector('.modal-title-2')
+    const modalBodyInput2 = exampleModal2.querySelector('.modal-body-2 input')
 
-    modalTitle2.textContent = `Compartilhe sua sugestão`
+    modalTitle2.textContent = `Compartilhe sua opinião conosco!`
     modalBodyInput2.value = recipient2
   })
 }
