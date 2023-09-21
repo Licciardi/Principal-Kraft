16d3d22e943c0876b95963723751be8a1f164a35
Carlos Henrique Lins Ponchirolli <107359480+CarlosHLP060304@users.noreply.github.com>
2023-09-21 15:40:27 -0300

    Add files via upload

--- JS/login.js
+++ JS/login.js
@@ -3,6 +3,7 @@ const confirm_senha = document.querySelector("#senha_confirm")
 const msg_senha_diferente = document.querySelector("#msg_senha_diferente")
 const container_data = document.querySelector("#container-data")
 const tipo_cliente = document.querySelectorAll(".tipo_cliente")
+const container_cnpj = document.querySelector("#container-cnpj")
 console.log(tipo_cliente)
 
 confirm_senha.addEventListener("blur",()=>{
@@ -16,7 +17,9 @@ tipo_cliente.forEach((campo)=>{campo.addEventListener("click",()=>{
     console.log(campo.value)
     if(campo.value == "empresa"){
         container_data.classList.add("none")
+        container_cnpj.classList.remove("none")
     }else{
         container_data.classList.remove("none")
+        container_cnpj.classList.add("none")
     }
 })})
